d3ed2f785b5803246ba17cba82c878ab129f685f
Francisco Meirinhos <fcmeirinhos@gmail.com>
2021-03-20 17:32:25 +0100

    Added high-order functions to intermediate.jl

--- presentations/intermediate.jl
+++ presentations/intermediate.jl
@@ -4,125 +4,262 @@
 using Markdown
 using InteractiveUtils
 
-# ╔═╡ 9f0c419c-7b51-11eb-1313-1f0e4af52ff5
-begin
-	using Plots
-	
-	dj = 0.06
-	
-	# ps = hcat([[[jz, j₊] ; kondo_rg([jz, j₊])] for j₊ in 0.0:dj:1.0 for jz in -dj-j₊:-dj:-1.0]...)
-	
-	plot(ps[[1,3],:], ps[[2,4],:];
-		xlims = (-1,dj),
-		ylims=(-dj,1),
-		framestyle = :origin,
-		legend=false)
-end
-
 # ╔═╡ 9ba8170a-7b50-11eb-0aac-adc493e1f386
 md"""
 # Higher order functions
+_A first taste of functional programming_
 
-A higher-order function is a function that takes other functions as arguments or returns a function as result.
+A higher-order function is a function that
+- takes other functions as arguments
+and/or
+- returns a function as result
+
+
+## Function composition and piping
+Many times in scientific code it's required that functions are chained together.
+
+This can be quite of eye sore
+```julia
+sqrt(abs(sum([1,2,3])))
+```
+
+Function composition `\circ`
+```julia
+(sqrt ∘ abs ∘ sum)([1,2,3])
+```
+
+and piping
+```julia
+sum([1,2,3]) |> abs |> sqrt
+```
+alleviate this problem for _unary_ (single-arugment) functions.
+"""
+
+# ╔═╡ c9862c2a-897e-11eb-3592-ada39cc0637b
+md"""
+#### Exercise: Becoming a pastry chef
+(Re)create some syntatic sugar such as
+- a function composition operator `∘` for _unary_ functions
+- a reverse pipe `<|`
+
+Note: infix operators such as `∘` need to be wrapped around `()` in function definitions for parsing reasons
+"""
+
+# ╔═╡ 44b9eb9c-8980-11eb-17b8-1937532d3a28
+#= md"""
+```julia
+(∘)(f::Function, g::Function) = (x...)->f(g(x...))
+
+(<|)(f::Function, args...) = f(args...)
+```
+""" =#
+
+# ╔═╡ af38a46c-8981-11eb-26de-35f9e487423c
+md"""
+To better understand these functions, it's better to look at their function signature
+
+### map
+`map: (a -> b) × [a] -> [b]` 
+
+`map: ((a × b × ...) -> c) × [a] × [b] × ... -> [c]`
+
+Map a function over the elements of a container and collect the results
+
+```julia
+map(x -> x + 1, [1, 2, 3]) == [2, 3, 4]
+map((x, y) -> x * y, [1,2,3], [1, 10, 100]) == [1, 20, 300]
+```
+
+### foreach
+`foreach: (a -> b) × [a] -> nothing` 
+
+Map a function over the elements of a container
+
+```julia
+foreach(println, [1, 2, 3]) # prints
+```
+
+### reduce
+`reduce: ((a × a) -> a) × [a] -> a`
+
+Can only use non-associative operations since you can't guarantee what
+`reduce(-,[1,2,3])` will get reduced to `(1-2)-3` or `1-(2-3)`
+
+```julia
+reduce(+, [1, 2, 3, 4]) == 10 # `sum`
+reduce(*, [1, 2, 3, 4]) == 24 # `factorial`
+reduce(*, [1, 2, 3, 4]; init=-1) == -24
+```
+
+### foldl and foldr
+`foldl: ((b × a) -> b) × [a] -> b`
+
+`foldr: ((a × b) -> b) × [a] -> b`
+
+Basically a left- and right- associative reduce.
+
+```julia
+foldl(=>, 1:4) == ((1 => 2) => 3) => 4
+foldr(=>, 1:4) == 1 => (2 => (3 => 4))
+```
+
+It doesn't end here: check also `filter`, `mapreduce` and possible multiple chains with `Transducers.jl`.
+"""
+
+# ╔═╡ aeed2776-8981-11eb-0546-f74714090a43
+md"""
+#### Exercise: Folding left and right
+
+- Define a function `sum²(n)` that takes an integer and performs the sum $ 1^2 + 2^2 + 3^2 + ... + n^2 $
+
+- Use a folding operator to find the minimum element in a container
+"""
+
+# ╔═╡ 0b0781fa-8996-11eb-1808-25a3d8fa7497
+#= md"""
+```julia
+sum²(n) = foldl((x,y) -> x + y^2, 1:n; init=0)
+
+min_fold(c) = foldl((x,y) -> x <= y ? x : y, c)
+```
+""" =#
+
+# ╔═╡ 4eb2573e-8998-11eb-2274-379a03bed49c
+md"""
+# Iteration Utilities
+For more examples see [here](https://docs.julialang.org/en/v1/base/iterators/)
+
+### zip
+Run multiple iterators at the same time, until any of them is exhausted
+```julia
+a = [1,2,3]
+b = (10,20,30)
+
+for z in zip(a,b)
+	println(z) # (1,10) ... (2, 20) ... (3, 30)
+end
+```
+
+Question: What is the mathematical operation equivalent of zipping?
+
+### enumerate
+An iterator that yields `(i, x)` where `i` is a counter starting at `1`, and `x` is the `i`-th value from the given iterator
+```julia
+a = [10, 20, 30]
+
+for (i, aᵢ) in enumerate(a)
+	println("The $i-th entry of a is $aᵢ)
+end
+```
+"""
+
+# ╔═╡ e5c840d8-8978-11eb-37fa-77fb05a3d821
+md"""
+# `using LinearAlgebra`
+"""
+
+# ╔═╡ 8ac22e72-8978-11eb-1674-e1b95403e215
+md"""
+# Performance
+
+## Profiling
+
+## Hardware
+
+## Threading
 """
 
 # ╔═╡ b58a861c-7b50-11eb-286d-c5a5dd03429f
 md"""
-### Fixed points
+# Fixed points
 
 A fixed point of a function is an element of the function's domain that is mapped to itself by the function.
 
 $ x,\;f(x),\;f \circ f(x),\;f \circ f \circ f(x),\;... \rightarrow x^*$
 
 Let's write a `Julia` function that finds the fixed point of some function `f`
-"""
 
-# ╔═╡ cb3f9392-7b50-11eb-04d4-2bbee8e0438d
+```julia
+
 # Identity operator
 const id = x -> x
 
-# ╔═╡ dbdd21a8-7b50-11eb-3b79-0ba82fecd7c1
 # Ideally... Why will it fail?
 function ideal_fixed_point(f::Function)
     fix(x) = f ∘ fix(x)
     return fix
 end
 
-# ╔═╡ e9028cc4-7b50-11eb-1212-632d91f3f917
-begin
-	# Need a stopping criterion
-	function fixed_point(f::Function)
-		function fix(xᵢ)
-			xᵢ₊₁ = f(xᵢ)
-			if xᵢ₊₁ == xᵢ
-				return xᵢ
-			else
-				return fix(xᵢ₊₁)
-			end
-		end
-		return fix
-	end
-
-	good_g = fixed_point(id)
-	@show good_g(3);
-	
-	# TODO: Write this function as a 1-liner!
-	fixed_point(f::Function) = nothing
-end
-
-# ╔═╡ e0037c1e-7b50-11eb-32f4-593f6dbe6322
 bad_g = fixed_point(id)
 # @show bad_g(3) why will it fail??
+```
 
-# ╔═╡ 436ced30-7b51-11eb-0443-ff78d6611555
-begin
-	# The equality condition is too strict for computations! Change it for an approximation (\approx)
-	approx_fixed_point(f::Function) = fix(xᵢ) = (xᵢ₊₁ = f(xᵢ)) ≈ xᵢ ? xᵢ : fix(xᵢ₊₁)
-
-	g = approx_fixed_point(id)
-	@show g(3);
+```julia
+# Need a stopping criterion
+function fixed_point(f::Function)
+	function fix(xᵢ)
+		xᵢ₊₁ = f(xᵢ)
+		if xᵢ₊₁ == xᵢ
+			return xᵢ
+		else
+			return fix(xᵢ₊₁)
+		end
+	end
+	return fix
 end
 
-# ╔═╡ 758c920c-7b51-11eb-2e87-e39f2eca37f9
-begin
-	# Add another stopping criteria to avoid getting stuck
-	approx_stop_fixed_point(f::Function; maxᵢ=10^4) = fix(xᵢ, i=1) = (xᵢ₊₁ = f(xᵢ)) ≈ xᵢ || i == maxᵢ ? xᵢ : fix(xᵢ₊₁, i+1)
-end
+good_g = fixed_point(id)
+@show good_g(3);
 
-# ╔═╡ 9704a6d6-7b51-11eb-1d4e-57d678e451b8
-begin
-	# Kondo poor-man's scaling equations
-	kondo_rg_eqs(x; dk=-10^-3) = x .+ dk * [-2 * x[2]^2, -2 * x[1] * x[2]]
-	kondo_rg = approx_stop_fixed_point(kondo_rg_eqs)
-end
+# TODO: Write this function as a 1-liner!
+fixed_point(f::Function) = nothing
+```
 
-# ╔═╡ b693098e-7b51-11eb-0ab9-399ac06a39d7
+```julia
+# The equality condition is too strict for computations! Change it for an approximation (\approx)
+approx_fixed_point(f::Function) = fix(xᵢ) = (xᵢ₊₁ = f(xᵢ)) ≈ xᵢ ? xᵢ : fix(xᵢ₊₁)
+
+g = approx_fixed_point(id)
+@show g(3);
+```
+
+```julia
+# Add another stopping criteria to avoid getting stuck
+approx_stop_fixed_point(f::Function; maxᵢ=10^4) = fix(xᵢ, i=1) = (xᵢ₊₁ = f(xᵢ)) ≈ xᵢ || i == maxᵢ ? xᵢ : fix(xᵢ₊₁, i+1)
+
+# Kondo poor-man's scaling equations
+kondo_rg_eqs(x; dk=-10^-3) = x .+ dk * [-2 * x[2]^2, -2 * x[1] * x[2]]
+kondo_rg = approx_stop_fixed_point(kondo_rg_eqs)
+```
+
+```julia
+using Plots
+
+dj = 0.06
+
+# ps = hcat([[[jz, j₊] ; kondo_rg([jz, j₊])] for j₊ in 0.0:dj:1.0 for jz in -dj-j₊:-dj:-1.0]...)
+
+plot(ps[[1,3],:], ps[[2,4],:];
+	xlims = (-1,dj),
+	ylims=(-dj,1),
+	framestyle = :origin,
+	legend=false)
+```
+
+```julia
 # Another example
 logistic_map(r) = approx_stop_fixed_point(x -> r * x * (1-x))
-
-# ╔═╡ d6cc24f6-7b51-11eb-128e-b395e6fb6b6e
-md"""
-### [Folding](https://en.wikipedia.org/wiki/Fold_(higher-order_function))
-
-Analyze a recursive data structure and through use of a given combining operation, recombine the results of recursively processing its constituent parts, building up a return value.
-"""
-
-# ╔═╡ d6cbb6c4-7b51-11eb-0beb-f90fa8e3f94b
-md"""
-### Reducing
+```
 """
 
 # ╔═╡ Cell order:
-# ╠═9ba8170a-7b50-11eb-0aac-adc493e1f386
-# ╠═b58a861c-7b50-11eb-286d-c5a5dd03429f
-# ╠═cb3f9392-7b50-11eb-04d4-2bbee8e0438d
-# ╠═dbdd21a8-7b50-11eb-3b79-0ba82fecd7c1
-# ╠═e0037c1e-7b50-11eb-32f4-593f6dbe6322
-# ╠═e9028cc4-7b50-11eb-1212-632d91f3f917
-# ╠═436ced30-7b51-11eb-0443-ff78d6611555
-# ╠═758c920c-7b51-11eb-2e87-e39f2eca37f9
-# ╠═9704a6d6-7b51-11eb-1d4e-57d678e451b8
-# ╠═9f0c419c-7b51-11eb-1313-1f0e4af52ff5
-# ╠═b693098e-7b51-11eb-0ab9-399ac06a39d7
-# ╠═d6cc24f6-7b51-11eb-128e-b395e6fb6b6e
-# ╠═d6cbb6c4-7b51-11eb-0beb-f90fa8e3f94b
+# ╟─9ba8170a-7b50-11eb-0aac-adc493e1f386
+# ╟─c9862c2a-897e-11eb-3592-ada39cc0637b
+# ╟─44b9eb9c-8980-11eb-17b8-1937532d3a28
+# ╟─af38a46c-8981-11eb-26de-35f9e487423c
+# ╟─aeed2776-8981-11eb-0546-f74714090a43
+# ╟─0b0781fa-8996-11eb-1808-25a3d8fa7497
+# ╟─4eb2573e-8998-11eb-2274-379a03bed49c
+# ╟─e5c840d8-8978-11eb-37fa-77fb05a3d821
+# ╟─8ac22e72-8978-11eb-1674-e1b95403e215
+# ╟─b58a861c-7b50-11eb-286d-c5a5dd03429f
